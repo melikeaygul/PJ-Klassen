6bd1e2bd1cad4b8a3a15f0bf80afed531211767f
melikeaygul <melikeaygul0810@gmail.com>
2025-03-18 16:20:27 +0100

    Update bis Class Login

---
+++ login_post.dart
@@ -0,0 +1,14 @@
+class Login {
+  String titel;
+  String text;
+  String mail;
+  String passwort;
+  String regist;
+  String forgetPasswort;
+  String log;
+  String icon;
+  String notRegist;
+
+  Login(this.titel, this.text, this.mail, this.passwort, this.regist,
+      this.forgetPasswort, this.log, this.icon, this.notRegist);
+}
---
+++ registration_post.dart
@@ -0,0 +1,12 @@
+class Registration {
+  String titel;
+  String text;
+  String mail;
+  String passwort;
+  String repeatPasswort;
+  String regist;
+  String alreadyLoggedIn;
+
+  Registration(this.titel, this.text, this.mail, this.passwort,
+      this.repeatPasswort, this.regist, this.alreadyLoggedIn);
+}
