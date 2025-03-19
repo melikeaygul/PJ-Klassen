f5cdc599d5ac8eec147f5f16b8f484d3b4618b39
melikeaygul <melikeaygul0810@gmail.com>
2025-03-19 14:54:34 +0100

    Setting Class

---
+++ settings_post.dart
@@ -0,0 +1,15 @@
+class Setting {
+  String help;
+  String dark;
+  String deleteAcc;
+  String privateSet;
+  String logout;
+
+  Setting(
+    this.help,
+    this.dark,
+    this.deleteAcc,
+    this.privateSet,
+    this.logout,
+  );
+}
