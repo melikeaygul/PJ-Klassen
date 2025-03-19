3633d08bed0bab27d2276ccfaa3660706812f05a
melikeaygul <melikeaygul0810@gmail.com>
2025-03-19 08:59:14 +0100

    contribution class

---
+++ contribution_post.dart
@@ -0,0 +1,14 @@
+class Contribution {
+  String name;
+  String id;
+  String postPicUrl;
+  int like;
+  int comment;
+  int share;
+  bool favorit;
+  String info;
+  String link;
+
+  Contribution(this.name, this.id, this.postPicUrl, this.like, this.comment,
+      this.share, this.favorit, this.info, this.link);
+}
